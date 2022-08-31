86fe61b25d112759d011050163c67f28ae625693
LE QUANG HOA <112528230+Le-Hoa@users.noreply.github.com>
2022-08-30 18:58:05 -0700

    homework

---
+++ main.cpp
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <stdio.h>
+
+/* run this program using the console pauser or add your own getch, system("pause") or input loop */
+
+int main(int argc, char *argv[])
+ {
+ 	int a , b , c , d, i;
+ 	a = 5;
+ 	b = 6;
+ 	c = 7;
+ 	d = 8;
+ 	i = 17;
+ 	printf("result = %d\n",++i % 7);
+ 	printf("result = %d\n",5*(c-3+d));
+ 	printf("result = %d\n",a*(b+c/d)-22);
+
+	return 0;
+}
